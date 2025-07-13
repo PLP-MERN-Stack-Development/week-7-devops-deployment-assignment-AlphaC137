c09c5d9a44f69f62bff6d0f84c18f1facf336848
Alpha C137 <alphasydwell93@gmail.com>
2025-07-13 18:56:45 +0000

    Add package.json and package-lock.json; implement MongoDB connection in server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// MongoDB connection
+const mongoURI = process.env.MONGODB_URI || 'mongodb://root:example@localhost:27017/mernapp?authSource=admin';
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
 app.get('/', (req, res) => {
-  res.send('Backend is running!');
+  res.send('Backend is running and connected to MongoDB!');
 });
 
 app.listen(PORT, () => {
